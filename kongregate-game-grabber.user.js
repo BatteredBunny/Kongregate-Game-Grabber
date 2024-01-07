39f38f403b7ae26445f4bbdc4acc76bf9b3b4aaf
BatteredBunny <ayes2022@protonmail.com>
2024-01-07 04:57:30 +0200

    Rename

--- kongregate-game-grabber.user.js
+++ kongregate-game-grabber.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         kongregate game grabber
 // @namespace    kongregate
-// @version      0.2
+// @version      0.3
 // @description  finds the direct links to web games on kongregate
-// @author       ayes-web
+// @author       BatteredBunny
 // @include      https://www.kongregate.com/games/*
-// @downloadURL  https://github.com/ayes-web/kongregate-game-grabber/raw/master/kongregate-game-grabber.user.js
+// @downloadURL  https://github.com/BatteredBunny/kongregate-game-grabber/raw/master/kongregate-game-grabber.user.js
 // ==/UserScript==
 
 (function() {
@@ -17,7 +17,7 @@
     info.innerHTML = "<a href='https://" + match[0] + "' target='_blank'><span style='color: #888' aria-hidden='true' class='kong_ico add prs'>+</span>Open file in new page</a>";
     info.classNames = "play_later lbOn"
     info.id = "quicklinks_play_later_block"
-  
+
     var game_table = document.getElementById("quicklinks");
     game_table.prepend(info);
 })();
